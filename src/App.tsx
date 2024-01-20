da6c98ec445363a241ff6f07f3a666a9dc42c73a
Richard <81489104+RichardTuharsky@users.noreply.github.com>
2024-01-20 17:48:51 +0100

    xx1x

--- src/App.tsx
+++ src/App.tsx
@@ -17,10 +17,30 @@ function App() {
   return (
     <>
       <GlobalStyles />
-      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh', background: '#ff6600' }}>
-        <h1 style={{ color: 'white', fontFamily: 'sans-serif', fontSize: '30px', justifyContent: "center", textAlign: "center", marginBottom: "450px" }}>
+      <div style={{ 
+        width: '100%', 
+        height: '50px', 
+        background: '#33CC33' // Change background color to light grey
+      }}></div> 
+      <div style={{ 
+        display: 'flex', 
+        flexDirection: 'column', 
+        alignItems: 'center', 
+        height: '100vh', 
+        background: '#ff6600',
+        paddingTop: '150px'  // Add padding at the top
+      }}>
+        <h1 style={{ 
+          color: 'white', 
+          fontFamily: 'sans-serif', 
+          fontSize: '40px', 
+          textAlign: "center", 
+          marginBottom: '0px', // Reduced margin
+          fontWeight: '1000', // Add fontWeight property
+        }}>
           Effortlessly craft stunning Midjourney collages in minutes, save <br /> hours of energy & headache!
         </h1>
+          <p style={{ fontSize: '30px', color: "white", marginBottom: "40px", textAlign: "center" }}>Streamline the collage creation for seamless results.</p>
         {!subscribed ? (
           <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
             <div style={{ display: 'flex', marginBottom: '10px' }}>
@@ -37,20 +57,20 @@ function App() {
                 }}
               />
               <button
-                onClick={handleSubscribe}
-                style={{
-                  padding: '10px 20px',
-                  background: '#33CC33 ',
-                  color: 'white',
-                  border: 'none',
-                  borderRadius: '4px',
-                  cursor: 'pointer',
-                  marginLeft: '10px',
-                  fontWeight: 'bold', // Added fontWeight property to make the text bold
-                }}
-              >
-                Subscribe
-              </button>
+                  onClick={handleSubscribe}
+                  style={{
+                    padding: '10px 20px',
+                    background: '#33CC33',
+                    color: 'white',
+                    border: 'none',
+                    borderRadius: '4px',
+                    cursor: 'pointer',
+                    marginLeft: '10px',
+                    fontWeight: 'bold', // Add fontWeight property
+                  }}  
+                >
+                  Subscribe
+                </button>
             </div>
           </div>
         ) : (
