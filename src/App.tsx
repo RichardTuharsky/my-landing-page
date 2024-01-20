77c252b7dae0997a9adb833c1ba87a6514550654
Richard <81489104+RichardTuharsky@users.noreply.github.com>
2024-01-20 20:37:50 +0100

    xx1x

--- src/App.tsx
+++ src/App.tsx
@@ -20,15 +20,20 @@ function App() {
       <div style={{ 
         width: '100%', 
         height: '50px', 
-        background: '#33CC33' // Change background color to light grey
-      }}></div> 
+        background: '#BC5E45',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+        <img src="/my-landing-page/src/assets/logo.png" alt="Logo" style={{ width: '30px', height: '30px' }} />
+      </div> 
       <div style={{ 
         display: 'flex', 
         flexDirection: 'column', 
         alignItems: 'center', 
         height: '100vh', 
         background: '#ff6600',
-        paddingTop: '150px'  // Add padding at the top
+        paddingTop: '150px'  
       }}>
         <h1 style={{ 
           color: 'white', 
@@ -38,9 +43,9 @@ function App() {
           marginBottom: '0px', // Reduced margin
           fontWeight: '1000', // Add fontWeight property
         }}>
-          Effortlessly craft stunning Midjourney collages in minutes, save <br /> hours of energy & headache!
+          Effortlessly craft stunning collages in minutes.
         </h1>
-          <p style={{ fontSize: '30px', color: "white", marginBottom: "40px", textAlign: "center" }}>Streamline the collage creation for seamless results.</p>
+          <p style={{ fontSize: '30px', color: "white", marginBottom: "40px", textAlign: "center" }}>Streamline the collage creation for seamless results and save <br /> hours of energy & headache!</p>
         {!subscribed ? (
           <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
             <div style={{ display: 'flex', marginBottom: '10px' }}>
@@ -50,33 +55,48 @@ function App() {
                 onChange={handleInputChange}
                 placeholder="Enter your email"
                 style={{
-                  padding: '10px',
+                  padding: '15px', // Increase padding
                   border: '1px solid #ccc',
                   borderRadius: '4px',
-                  width: '300px',
+                  width: '400px', // Increase width
+                  fontSize: '16px', // Increase font size
                 }}
               />
               <button
-                  onClick={handleSubscribe}
-                  style={{
-                    padding: '10px 20px',
-                    background: '#33CC33',
-                    color: 'white',
-                    border: 'none',
-                    borderRadius: '4px',
-                    cursor: 'pointer',
-                    marginLeft: '10px',
-                    fontWeight: 'bold', // Add fontWeight property
-                  }}  
-                >
-                  Subscribe
-                </button>
+                onClick={handleSubscribe}
+                style={{
+                  padding: '15px 30px', // Increase padding
+                  background: '#33CC33',
+                  color: 'white',
+                  border: 'none',
+                  borderRadius: '4px',
+                  cursor: 'pointer',
+                  marginLeft: '10px',
+                  fontWeight: 'bold',
+                  fontSize: '16px', // Increase font size
+                }}
+              >
+                Subscribe
+              </button>
             </div>
           </div>
         ) : (
-          <p>Thank you for your subscription!</p>
+          <p style={{color: "white", fontWeight: "bold"}}>Thank you for your subscription!</p>
         )}
       </div>
+      <div style={{ 
+        width: '100%', 
+        height: '50px', 
+        background: 'rgb(17,19,23)', // Change background color to light grey
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        color: 'white',
+        fontSize: '14px',
+      }}>
+        <img src="/assets/logo.png" alt="Artfuly-Logo" style={{ width: '30px', height: '30px', marginRight: '10px' }} />
+      <p style={{ fontSize: '13px', color: "grey", textAlign: "left" }}>2024 Artfuly. All rights reserved.</p>
+      </div> 
     </>
   );
 }
