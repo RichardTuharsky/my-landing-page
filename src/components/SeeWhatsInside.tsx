b37ebd505a97a425f57696481ecb7262de9ce249
Richard <81489104+RichardTuharsky@users.noreply.github.com>
2024-01-21 13:25:48 +0100

    xx1x

--- src/components/SeeWhatsInside.tsx
+++ src/components/SeeWhatsInside.tsx
@@ -8,27 +8,36 @@ const SeeWhatsInside: React.FC = () => {
     setSelectedFeature(feature);
   };
 
+
+  const getImageSrc = (feature: string) => {
+    const baseSrc: { [key: string]: string } = {
+      'collage': 'Collage',
+      'watermark': 'watermark',
+      'size': 'Sizing'
+    };
+
+    return selectedFeature === feature ? `${baseSrc[feature]}_color.png` : `${baseSrc[feature]}.png`;
+  };
+
+
   return (
     <div className="container">
       <div className="options">
         <div className="option" onClick={() => handleFeatureClick('collage')}>
-          {/* Replace these placeholders with actual icons */}
-          <img src="Collage.png" alt="Collage" style={{ width: '80px', height: '80px'}} />
-          <p style={{color: 'gray'}}>Collage</p>
+          <img src={getImageSrc('collage')} alt="Collage" style={{ width: '80px', height: '80px' }} />
+          <p style={{color: selectedFeature === 'collage' ? 'black' : 'gray'}}>Collage</p>
           {selectedFeature === 'collage' && <p>Description of Collage feature...</p>}
         </div>
 
         <div className="option" onClick={() => handleFeatureClick('watermark')}>
-          {/* Replace these placeholders with actual icons */}
-          <img src="watermark.png" alt="Watermark" style={{ width: '80px', height: '80px'}}/>
-          <p style={{color: 'gray'}}>Watermark</p>
+          <img src={getImageSrc('watermark')} alt="Watermark" style={{ width: '80px', height: '80px' }} />
+          <p style={{color: selectedFeature === 'watermark' ? 'black' : 'gray'}}>Watermark</p>
           {selectedFeature === 'watermark' && <p>Description of Watermark feature...</p>}
         </div>
 
         <div className="option" onClick={() => handleFeatureClick('size')}>
-          {/* Replace these placeholders with actual icons */}
-          <img src="Sizing.png" alt="Sizing"style={{ width: '60px', height: '60px', marginTop: '13px'}}/>
-          <p style={{color: 'gray'}}>Sizing</p>
+          <img src={getImageSrc('size')} alt="Sizing" style={{ width: '60px', height: '60px', marginTop: '13px' }} />
+          <p style={{color: selectedFeature === 'size' ? 'black' : 'gray'}}>Sizing</p>
           {selectedFeature === 'size' && <p>Description of Collage Size feature...</p>}
         </div>
       </div>
